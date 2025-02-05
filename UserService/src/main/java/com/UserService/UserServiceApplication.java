1ed53b8e9c59aded34f2ecfd02a5c611c7108079
SUDIPTA252002 <sudipta.mahato.ece25@heritageit.edu.in>
2025-02-05 19:23:48 +0530

    implemented eureka discovery client for UserService

--- UserService/src/main/java/com/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/UserService/UserServiceApplication.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
